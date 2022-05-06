45bccc4c03b256dca3eb8aa2d5cb71487f25eea9
dardoschi <davidespada420@gmail.com>
2022-05-06 12:27:41 +0200

    aggiunti metadata al pacchetto dati

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import camera_multiprocess
 from camera_operations import populate_camera_list
 
 processes_list = []
-
+queue = multiprocessing.Queue()
 camera_list = []
 
 populate_camera_list(camera_list)
 
 for camera in camera_list:
-    queue = multiprocessing.Queue()
     print("trying to access ---"+camera.ip+ camera.port+ camera.user+camera.passw)
-    proc = multiprocessing.Process(target = camera_multiprocess.camera_process_func,args=(camera.ip, camera.port, camera.user, camera.passw))
+    proc = multiprocessing.Process(target = camera_multiprocess.camera_process_func,args=(queue, camera.ip, camera.port, camera.user, camera.passw))
     processes_list.append(proc)
 
 for proc in processes_list:
